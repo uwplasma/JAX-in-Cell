4704374aee52da401d2106998a884c8708cfcfdf
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2024-12-03 22:07:20 -0600

    Now working for both plasma_oscillations and two_stream

--- diagnostics.py
+++ diagnostics.py
@@ -1,17 +1,12 @@
+from jax import vmap
+from constants import epsilon_0
 import jax.numpy as jnp
 from jax.numpy.fft import fft, fftfreq
-from constants import epsilon_0, charge_electron, mass_electron
-
-def system_parameters(output):
-    no_pseudoelectrons = output['number_pseudoelectrons']
-    weight = output['weight']
-    length = output['length']
-    plasma_frequency = jnp.sqrt(no_pseudoelectrons * weight * charge_electron**2)/jnp.sqrt(mass_electron)/jnp.sqrt(epsilon_0)/jnp.sqrt(length)
-    return plasma_frequency
+from constants import epsilon_0, charge_electron, mass_electron, mass_proton, speed_of_light, mu_0
+import matplotlib.pyplot as plt
 
 def diagnostics(output):
-    # charge_density_over_time = output['charge_density']
-    E_field_over_time = output['E_field']
+    E_field_over_time = output['electric_field']
     grid = output['grid']
     dt = output['dt']
     total_steps = output['total_steps']
@@ -19,7 +14,7 @@ def diagnostics(output):
     # array_to_do_fft_on = charge_density_over_time[:,len(grid)//2]
     array_to_do_fft_on = E_field_over_time[:,len(grid)//2,0]
     array_to_do_fft_on = (array_to_do_fft_on-jnp.mean(array_to_do_fft_on))/jnp.max(array_to_do_fft_on)
-    plasma_frequency = system_parameters(output)
+    plasma_frequency = jnp.sqrt(output['number_pseudoelectrons'] * output['weight'] * charge_electron**2)/jnp.sqrt(mass_electron)/jnp.sqrt(epsilon_0)/jnp.sqrt(output['length'])
 
     fft_values = fft(array_to_do_fft_on)[:total_steps//2]
     freqs = fftfreq(total_steps, d=dt)[:total_steps//2]*2*jnp.pi # d=dt specifies the time step
@@ -30,3 +25,23 @@ def diagnostics(output):
     print(f"Dominant FFT frequency (f): {dominant_frequency} Hz")
     print(f"Plasma frequency (w_p):     {plasma_frequency} Hz")
     print(f"Error: {jnp.abs(dominant_frequency - plasma_frequency) / plasma_frequency * 100:.2f}%")
+    
+    abs_E_squared = jnp.sum(output['electric_field']**2, axis=-1)
+    integral_E_squared = jnp.trapezoid(abs_E_squared, dx=output['dx'], axis=1)
+    
+    abs_B_squared = jnp.sum(output['magnetic_field']**2, axis=-1)
+    integral_B_squared = jnp.trapezoid(abs_B_squared, dx=output['dx'], axis=1)
+    
+    v_electrons_squared = jnp.sum(jnp.sum(output['velocity_electrons']**2, axis=-1), axis=-1)
+    v_ions_squared      = jnp.sum(jnp.sum(output['velocity_ions']**2     , axis=-1), axis=-1)
+    output.update({
+        'electric_field_energy_density': (epsilon_0/2) * abs_E_squared,
+        'electric_field_energy':         (epsilon_0/2) * integral_E_squared,
+        'magnetic_field_energy_density': 1/(2*mu_0)    * abs_B_squared,
+        'magnetic_field_energy':         1/(2*mu_0)    * integral_B_squared,
+        'dominant_frequency': dominant_frequency,
+        'plasma_frequency':   plasma_frequency,
+        'kinetic_energy':     (1/2) * mass_electron * output['weight'] * v_electrons_squared + (1/2) * mass_proton * output['weight'] * v_ions_squared,
+        'kinetic_energy_electrons': (1/2) * mass_electron * output['weight'] * v_electrons_squared,
+        'kinetic_energy_ions':      (1/2) * mass_proton   * output['weight'] * v_ions_squared,
+    })
--- plot.py
+++ plot.py
@@ -1,20 +1,126 @@
+from jax import vmap
+import jax.numpy as jnp
 import matplotlib.pyplot as plt
+from constants import speed_of_light, mass_proton, mass_electron
+from matplotlib.animation import FuncAnimation
 
 def plot_results(output):
-    plt.figure()
-    plt.imshow(output["E_field"][:, :, 0], aspect='auto', cmap='RdBu', origin='lower', extent=[output["grid"][0], output["grid"][-1], 0, output["total_steps"] * output["dt"]])
-    plt.colorbar(label='Electric field (V/m)')
-    plt.xlabel('Position (m)')
-    plt.ylabel('Time (s)')
+    v_th = output["vth_electrons_over_c"] * speed_of_light
+    grid = output["grid"]
+    box_size_x = output["length"]
+    total_steps = output["total_steps"]
+    sqrtmime = jnp.sqrt(mass_proton/mass_electron)
+    max_velocity_electrons = max(1.2*jnp.max(output['velocity_electrons']),                                       5 * jnp.abs(v_th) + jnp.abs(output["electron_drift_speed"]))
+    max_velocity_ions      = max(1.2*jnp.max(output['velocity_ions']     ), jnp.sqrt(mass_electron/mass_proton) * 5 * jnp.abs(v_th) + jnp.abs(output["ion_drift_speed"]))
+    
+    fig, axes = plt.subplots(2, 4, figsize=(16, 7))
+    plt.subplots_adjust(hspace=0.7, wspace=0.4)
 
-    plt.figure()
-    plt.imshow(output["charge_density"], aspect='auto', cmap='RdBu', origin='lower', extent=[output["grid"][0], output["grid"][-1], 0, output["total_steps"] * output["dt"]])
-    plt.colorbar(label='Charge density (C/m^3)')
-    plt.xlabel('Position (m)')
-    plt.ylabel('Time (s)')
+    # Electric field plot
+    im1 = axes[0, 0].imshow(
+        output["electric_field"][:, :, 0], aspect='auto', cmap='RdBu', origin='lower',
+        extent=[output["grid"][0], output["grid"][-1], 0, total_steps * output["dt"]]
+    )
+    axes[0, 0].set(title='Electric Field', xlabel='Position (m)', ylabel='Time (s)')
+    fig.colorbar(im1, ax=axes[0, 0], label='Electric field (V/m)')
 
-    plt.figure()
-    plt.plot(output["time_array"], output["E_energy"])
-    plt.xlabel('Time (s)')
-    plt.ylabel('Electric field energy (J)')
+    # Charge density plot
+    im2 = axes[0, 1].imshow(
+        output["charge_density"], aspect='auto', cmap='RdBu', origin='lower',
+        extent=[output["grid"][0], output["grid"][-1], 0, total_steps * output["dt"]]
+    )
+    axes[0, 1].set(title='Charge Density', xlabel='Position (m)', ylabel='Time (s)')
+    fig.colorbar(im2, ax=axes[0, 1], label='Charge density (C/m³)')
+    
+    # Mean charge density
+    total_energy = output["electric_field_energy"] + output["magnetic_field_energy"] + output["kinetic_energy"]
+    axes[0, 2].plot(output["time_array"], jnp.abs(jnp.mean(output["charge_density"], axis=-1)), label='Mean charge density (C/m³)')
+    axes[0, 2].plot(output["time_array"], jnp.abs(total_energy-total_energy[0])/total_energy[0], label='Relative energy error')
+    axes[0, 2].set(title='Mean Charge Density and Energy Error', xlabel='Time (s)', ylabel='Charge density/Energy error', yscale='log')
+    axes[0, 2].legend()
+
+    # Electric field energy
+    axes[0, 3].plot(output["time_array"], output["electric_field_energy"], label='Electric field energy')
+    axes[0, 3].plot(output["time_array"], output["kinetic_energy"], label='Kinetic energy')
+    axes[0, 3].plot(output["time_array"], output["kinetic_energy_electrons"], label='Kinetic energy electrons')
+    axes[0, 3].plot(output["time_array"], output["kinetic_energy_ions"], label='Kinetic energy ions')
+    axes[0, 3].plot(output["time_array"], total_energy, label='Total energy')
+    axes[0, 3].set(title='Energy', xlabel='Time (s)', ylabel='Energy (J)')
+    axes[0, 3].legend()
+    
+    # Find the maximum histogram value across all frames for both electrons and ions
+    bins = jnp.linspace(-box_size_x / 2, box_size_x / 2, len(grid) + 1)
+    electron_histograms = vmap(lambda x: jnp.histogram(x, bins=bins)[0])(output['position_electrons'][:, :, 0])
+    ion_histograms      = vmap(lambda x: jnp.histogram(x, bins=bins)[0])(output['position_ions'][:, :, 0])
+    max_y_all_frames_positions = jnp.max(jnp.concatenate([electron_histograms, ion_histograms]))
+    
+    bins_electrons = jnp.linspace(-max_velocity_electrons, max_velocity_electrons, len(grid) + 1)
+    bins_ions      = jnp.linspace(-max_velocity_ions     , max_velocity_ions     , len(grid) + 1)
+    electron_histograms = vmap(lambda x: jnp.histogram(x, bins=bins_electrons)[0])(output['velocity_electrons'][:, :, 0])
+    ion_histograms      = vmap(lambda x: jnp.histogram(x, bins=bins_ions     )[0])(output['velocity_ions'][:, :, 0])
+    max_y_all_frames_velocities = jnp.max(jnp.concatenate([electron_histograms, ion_histograms]))
+
+    # Animated phase space for electrons
+    def update(frame):
+        # Distribution of electrons and ions
+        axes[1, 0].clear()
+        axes[1, 0].hist(output['position_electrons'][frame, :, 0], jnp.linspace(-box_size_x / 2, box_size_x / 2, len(grid) + 1), color='red' , label='electrons', alpha=0.5)
+        axes[1, 0].hist(output['position_ions'][frame, :, 0]     , jnp.linspace(-box_size_x / 2, box_size_x / 2, len(grid) + 1), color='blue', label='ions'     , alpha=0.5)
+        axes[1, 0].set(title=f'Positions at timestep {frame}/{total_steps}', xlabel='Position (m)', ylabel='Number of particles')
+        axes[1, 0].set_xlim(-box_size_x / 2, box_size_x / 2)
+        axes[1, 0].set_ylim(0, max_y_all_frames_positions)
+        axes[1, 0].legend(loc='lower right')
+        
+        axes[1, 1].clear()
+        axes[1, 1].hist(         output['velocity_electrons'][frame, :, 0], jnp.linspace(-max_velocity_electrons    , max_velocity_electrons    , len(grid) + 1), color='red' , label='electrons'      , alpha=0.5)
+        axes[1, 1].hist(sqrtmime*output['velocity_ions'     ][frame, :, 0], jnp.linspace(-max_velocity_ions*sqrtmime, max_velocity_ions*sqrtmime, len(grid) + 1), color='blue', label='ions*mass_ratio', alpha=0.5)
+        axes[1, 1].set(title=f'Velocity Distribution at timestep {frame}/{total_steps}', xlabel='Velocity (m/s)', ylabel='Number of particles')
+        axes[1, 1].set_ylim(0, max_y_all_frames_velocities)
+        axes[1, 1].set_xlim(-1.2*max(max_velocity_electrons, sqrtmime*max_velocity_ions), 1.2*max(max_velocity_electrons, sqrtmime*max_velocity_ions))
+        axes[1, 1].legend(loc='upper right')
+        
+        axes[1, 2].clear()
+        h = axes[1, 2].hist2d(
+            output['position_electrons'][frame, :, 0],
+            output['velocity_electrons'][frame, :, 0],
+            bins=jnp.array([len(grid), 50]),
+            range=[[-box_size_x / 2, box_size_x / 2], [-max_velocity_electrons, max_velocity_electrons]],
+            cmap='plasma'
+        )
+        # axes[1, 2].set(title=f'Phase Space of Electrons at timestep {frame}/{total_steps}')
+        axes[1, 2].set(xlabel='Electron Position (m)', ylabel='Electron Velocity (m/s)')
+
+        axes[1, 3].clear()
+        h = axes[1, 3].hist2d(
+            output['position_ions'][frame, :, 0],
+            output['velocity_ions'][frame, :, 0],
+            bins=jnp.array([len(grid), 50]),
+            range=[[-box_size_x / 2, box_size_x / 2], [-max_velocity_ions, max_velocity_ions]],
+            cmap='viridis'
+        )
+        # axes[1, 3].set(title=f'Phase Space of Ions at timestep {frame}/{total_steps}')
+        axes[1, 3].set(xlabel='Ion Position (m)', ylabel='Ion Velocity (m/s)')
+
+    ani = FuncAnimation(fig, update, frames=output["total_steps"], blit=False, interval=1, repeat_delay=1000)
+
+    # Add static colorbars for context
+    h_electrons = axes[1, 2].hist2d(
+        output['position_electrons'][0, :, 0],
+        output['velocity_electrons'][0, :, 0],
+        bins=jnp.array([len(grid), 50]),
+        range=[[-box_size_x / 2, box_size_x / 2], [-max_velocity_electrons, max_velocity_electrons]],
+        cmap='plasma'
+    )
+    fig.colorbar(h_electrons[3], ax=axes[1, 2], label='Number of electrons')
+
+    h_ions = axes[1, 3].hist2d(
+        output['position_ions'][0, :, 0],
+        output['velocity_ions'][0, :, 0],
+        bins=jnp.array([len(grid), 50]),
+        range=[[-box_size_x / 2, box_size_x / 2], [-max_velocity_ions, max_velocity_ions]],
+        cmap='viridis'
+    )
+    fig.colorbar(h_ions[3], ax=axes[1, 3], label='Number of ions')
+
+    plt.tight_layout()
     plt.show()
