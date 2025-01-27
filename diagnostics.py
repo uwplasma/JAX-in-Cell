c00f5997f4a01d11a5051d00cdfed776f954702d
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2025-01-26 20:55:00 -0600

    Add differentiability test and optimize FFT calculation in diagnostics

--- diagnostics.py
+++ diagnostics.py
@@ -1,4 +1,4 @@
-from jax import vmap
+from jax import vmap, lax
 from constants import epsilon_0
 import jax.numpy as jnp
 from jax.numpy.fft import fft, fftfreq
@@ -16,7 +16,7 @@ def diagnostics(output, print_to_terminal=True):
     array_to_do_fft_on = (array_to_do_fft_on-jnp.mean(array_to_do_fft_on))/jnp.max(array_to_do_fft_on)
     plasma_frequency = output['plasma_frequency']
 
-    fft_values = fft(array_to_do_fft_on)[:total_steps//2]
+    fft_values = lax.slice(fft(array_to_do_fft_on), (0,), (total_steps//2,))
     freqs = fftfreq(total_steps, d=dt)[:total_steps//2]*2*jnp.pi # d=dt specifies the time step
     magnitude = jnp.abs(fft_values)
     peak_index = jnp.argmax(magnitude)
