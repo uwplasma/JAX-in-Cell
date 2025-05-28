884eff7e5250b0d8d63ce7d7ca72206b03d1f413
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2025-05-27 23:56:26 -0500

    Fixed Landau damping

--- examples/Landau_damping.py
+++ examples/Landau_damping.py
@@ -8,7 +8,7 @@ from jax import block_until_ready
 input_parameters = {
 "length"                       : 1e-1,  # dimensions of the simulation box in (x, y, z)
 "amplitude_perturbation_x"     : 1e-1,  # amplitude of sinusoidal perturbation in x
-"wavenumber_electrons": 1, # Wavenumber of sinusoidal electron density perturbation in x (factor of 2pi/length)
+"wavenumber_electrons_x": 1, # Wavenumber of sinusoidal electron density perturbation in x (factor of 2pi/length)
 "grid_points_per_Debye_length" : 0.3,     # dx over Debye length
 "velocity_plus_minus_electrons_x": False,    # create two groups of electrons moving in opposite directions
 "ion_temperature_over_electron_temperature_x": 1e-6, # Temperature of ions over temperature of electrons
