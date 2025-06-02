Fixed gamma factor calculation in simulation

jaxincell/_plot.py
@@ -159,7 +159,7 @@ def plot(output, direction="x", threshold=1e-12):
         energy_ax.plot(time, output["electric_field_energy"], label="Electric field energy")
         if jnp.max(output["magnetic_field_energy"]) > 1e-10:
             energy_ax.plot(time, output["magnetic_field_energy"], label="Magnetic field energy")
-        energy_ax.plot(time[2:], jnp.abs(jnp.mean(output["charge_density"][2:], axis=-1))*1e12, label=r"Mean $\rho \times 10^{12}$")
+        energy_ax.plot(time[2:], jnp.abs(jnp.mean(output["charge_density"][2:], axis=-1))*1e15, label=r"Mean $\rho \times 10^{15}$")
         energy_ax.plot(time[2:], jnp.abs(output["total_energy"][2:] - output["total_energy"][2]) / output["total_energy"][2], label="Relative energy error")
         energy_ax.set(title="Energy", xlabel=r"Time ($\omega_{pe}^{-1}$)",
                     ylabel="Energy (J)", yscale="log", ylim=[1e-7, None])

jaxincell/_simulation.py
@@ -285,7 +285,7 @@ def initialize_particles_fields(input_parameters={}, number_grid_points=50, numb
 
     # Print information about the simulation
     plasma_frequency = jnp.sqrt(number_pseudoelectrons * weight * charge_electrons**2)/jnp.sqrt(mass_electrons)/jnp.sqrt(epsilon_0)/jnp.sqrt(length)
-    relativistic_gamma_factor = 1/jnp.sqrt(1 - velocities / speed_of_light)**2
+    relativistic_gamma_factor = 1 / jnp.sqrt(1 - jnp.sum(velocities**2, axis=1) / speed_of_light**2)
 
     cond(parameters["print_info"],
         lambda _: jprint((